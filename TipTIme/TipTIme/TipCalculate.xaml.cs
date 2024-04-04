751a73827b9e47995d45b4c7df4da9d3ee9b3611
Raphael567 <raphaelcanuto464@gmail.com>
2024-04-04 12:44:11 -0300

    Atualizando Arquivos

--- TipTIme/TipTIme/TipCalculate.xaml.cs
+++ TipTIme/TipTIme/TipCalculate.xaml.cs
@@ -6,4 +6,53 @@ public partial class TipCalculate : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button15Percentage_Clicked(object sender, EventArgs e)
+    {
+        TipPercentageSlider.Value = 15;
+    }
+
+    private void Button20Percentage_Clicked(object sender, EventArgs e)
+    {
+        TipPercentageSlider.Value = 20;
+    }
+
+    private void RoundDownButton_Clicked(object sender, EventArgs e)
+    {
+        // Arredondar o número para o intei8ro menor ao valor
+        // Pegar o valor da refeição
+        // Pegar a porcentagem que o usuário selecionou
+        // Calcular o valor da gorjeta
+        // Arredondar
+        // Calcular o total
+        // Exibir
+
+        double valorDaRefeição = Convert.ToDouble(ValueEntry.Text);
+        double porcentagemDaGorjeta = TipPercentageSlider.Value/100;
+        double valorDaGorjeta = valorDaRefeição * porcentagemDaGorjeta;
+        valorDaGorjeta = Math.Floor(valorDaGorjeta);
+        double totalRefeição = valorDaGorjeta + valorDaRefeição;
+
+        TotalLabel.Text = totalRefeição.ToString();
+        TipLabel.Text = valorDaGorjeta.ToString();
+    }
+
+    private void RoundUpButton_Clicked(object sender, EventArgs e)
+    {
+        double valorDaRefeição = Convert.ToDouble(ValueEntry.Text);
+        double porcentagemDaGorjeta = TipPercentageSlider.Value / 100;
+        double valorDaGorjeta = valorDaRefeição * porcentagemDaGorjeta;
+        valorDaGorjeta = Math.Ceiling(valorDaGorjeta);
+        double totalRefeição = valorDaGorjeta + valorDaRefeição;
+
+        TotalLabel.Text = totalRefeição.ToString();
+        TipLabel.Text = valorDaGorjeta.ToString();
+
+    }
+
+    private void TipPercentageSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        double valorDoSlider = TipPercentageSlider.Value;
+        PercentageLabel.Text = valorDoSlider.ToString() + "%"; 
+    }
 }
